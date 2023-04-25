8e8f1345eb73fe19114635c51fc165f02472c32e
Rohit-Baviskar <90634897+Rohit-Baviskar@users.noreply.github.com>
2023-04-25 17:56:36 +0530

    Update main.tf

--- AWS/main.tf
+++ AWS/main.tf
@@ -2,10 +2,29 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "ec2-user" {
-  ami           = "ami-06fc49795bc410a0c" # us-west-2
+resource "aws_instance" "ubuntu" {
+  ami           = "ami-02eb7a4783e7e9317"
   instance_type = "t2.micro"
+
   tags = {
-      Name = "Server 1"
+    Name = "server-1"
+  }
+}
+
+resource "aws_instance" "ec2-user" {
+  ami           = "ami-0c768662cc797cd75"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-2"
+  }
+}
+
+resource "aws_instance" "redhat" {
+  ami           = "ami-0fdea1353c525c182"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "server-3"
   }
 }
